a359987a4979a678e1e10219c341cc99738b8206
Zikboy <43441041+akashkinKV@users.noreply.github.com>
2019-06-24 22:59:10 +0000

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,19 +1,29 @@
 package hello;
 
-import java.util.Arrays;
+import java.util.*;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
-
-
-import java.util.Comparator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -38,7 +48,13 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 @SpringBootApplication
 public class Application extends TelegramLongPollingBot {
 public String URL;
+    public  String URL_API="https://beatmaker.tv/mp3/Mp3/ValidateCode";
+
+    public static String CID="d256c775-88b6-47b9-a313-958228f49485";
 public int schet=0;
+    public  static boolean start=true;
+    public  static List<String> idList=new ArrayList<>();
+
     public static void main(String[] args) {
         ApiContextInitializer.init();
         SpringApplication.run(Application.class, args);
@@ -57,18 +73,50 @@ public int schet=0;
     public class ScheduledTasks {
 
 
-        @Scheduled(cron = "*/120 * * * * *")
+
+        @Scheduled(cron = "*/10 * * * * *")
         public void reportCurrentTime() {
+        if(start==true) {
+            schet = schet + 1;
 
-                schet=schet+1;
+            try {
 
-               SendMessage sendMessage = new SendMessage();
-               sendMessage.enableMarkdown(true);
-               sendMessage.setChatId("416744346");
-               sendMessage.setText(Integer.toString(schet));
-               sendMessageMain(sendMessage);
+                UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(URL_API);
+                System.out.println(start);
+                System.out.println(idList.size());
+                for (String item : idList) {
+                    System.out.println(item);
+                    Formatter f = new Formatter();
+                    String requestJson = f.format("code=%s&beatId=%s&isBeat=true&isFreeBeat=false",CID,item).toString();
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+
+                    HttpEntity<String> entity = new HttpEntity<>(requestJson, headers);
+                    RestTemplate restTemplate = new RestTemplate();
+
+                    String result =
+                            restTemplate.exchange(
+                                    builder.toUriString(), HttpMethod.POST, entity, String.class).getBody();
+                }
 
 
+            } catch (HttpServerErrorException e) {
+
+                SendMessage sendMessage = new SendMessage();
+                sendMessage.enableMarkdown(true);
+                sendMessage.setChatId("416744346");
+                sendMessage.setText(e.toString());
+                sendMessageMain(sendMessage);
+            }
+
+
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.enableMarkdown(true);
+            sendMessage.setChatId("416744346");
+            sendMessage.setText(Integer.toString(schet));
+            sendMessageMain(sendMessage);
+
+        }
         }
     }
     @Override
@@ -102,6 +150,47 @@ public int schet=0;
                         sendMessageMain(sendMessage);
                         URL=message.getChatId().toString();
                          break;
+
+                    case "start":
+                        start=true;
+                        break;
+                    case "stop":
+                        start=false;
+                        break;
+                    case "cid":
+                        SendMessage sendMessage2 = new SendMessage();
+                        sendMessage2.enableMarkdown(true);
+                        sendMessage2.setChatId(message.getChatId().toString());
+                        sendMessage2.setText(CID);
+                        sendMessageMain(sendMessage2);
+                        URL=message.getChatId().toString();
+                        break;
+                    case "clean":
+                        idList.clear();
+                        break;
+                    default:
+            if(message.getText().length()==36)
+            {
+                CID=message.getText();
+            }
+            else
+                {
+
+                    if(message.getText().length()>10)
+                    {
+                        System.out.println(message.getText());
+                        String[] subStr;
+                        String delimeter = "\n"; // Разделитель
+                        subStr = message.getText().split(delimeter); // Разделения строки str с помощью метода split()
+                        // Вывод результата на экран
+                        for (int i = 0; i < subStr.length; i++) {
+                            idList.add(subStr[i]);
+                        }
+                    }
+                }
+
+
+                        break;
 
 
                 }
